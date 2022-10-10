2e0c2480fd4f8fb9c7528cee49dbb82390f37d38
Miguel Urena <urena_miguel82@outlook.com>
2022-10-10 16:00:38 -0700

    Setup app

---
+++ src/images/images.js
@@ -0,0 +1,15 @@
+import dirtImg from './dirt.jpg';
+import grassImg from './grass.jpg';
+import glassImg from './glass.jpg';
+import logImg from './log.jpg';
+import woodImg from './wood.jpg';
+
+const images = {
+    dirtImg,
+    grassImg,
+    glassImg,
+    woodImg,
+    logImg,
+}
+
+export default images
