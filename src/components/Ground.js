194d3787c585f3a6b1169a9f81fca72b7e64f729
Miguel Urena <urena_miguel82@outlook.com>
2022-10-11 14:57:25 -0700

    Ground.js

---
+++ src/components/Ground.js
@@ -0,0 +1,15 @@
+import { usePlane } from "@react-three/cannon";
+import { groundTexture } from "../images/textures";
+
+export const Ground = () => {
+    const [ref] = usePlane(() => ({
+        rotation: [0,0,0], position:[0,0,0]
+    }))
+
+    return (
+        <mesh ref={ref}>
+            <planeBufferGeometry attach='geometry' args={[100,100]} />
+            <meshStandardMaterial attach='material' map={groundTexture} />
+        </mesh>
+    )
+}
